da339107c7b6da11a58b5b2b941e8e2eb4f4fcea
Alexandr <glualex066@gmail.com>
2025-05-28 18:41:11 +0300

    Запуск в докере, в облаке

--- CenterService/src/main/java/org/alexgls/centerservice/config/CorsConfiguration.java
+++ CenterService/src/main/java/org/alexgls/centerservice/config/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package org.alexgls.centerservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,10 +12,13 @@ public class CorsConfiguration {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
 
+            @Value("${production.url}")
+            private String productionUrl;
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:80,http://localhost")
+                        .allowedOrigins("http://localhost:80", "http://localhost", productionUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
